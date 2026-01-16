75c979303d0e1e841c937bda53d3fc362dc5c5fe
LeszczynskiKarol <leszczu123@wp.pl>
2026-01-16 12:06:19 +0100

    check

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://www.uniatorun.pl",
@@ -9,7 +8,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    sitemap(),
   ],
   build: {
     inlineStylesheets: "auto",
